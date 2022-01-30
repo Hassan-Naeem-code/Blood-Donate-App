0b036373b394d234cd4e66cdf326f0780b4c9f26
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-01-30 21:33:30 +0500

    changes fixes

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
